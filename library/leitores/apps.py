2174f7e480395919aeca967d53bb7f09c29e5d22
Andrey Masiero <andreymasiero@gmail.com>
2020-10-09 14:17:04 -0300

    Modelos e Integrações

--- library/leitores/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeitoresConfig(AppConfig):
-    name = 'leitores'
--- library/library/settings.py
+++ library/library/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livros',
-    'leitores'
+    'autores'
 ]
 
 MIDDLEWARE = [
