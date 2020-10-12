1e499bdca4b8ee6391338db50f1d5bc6d77aa076
Andrey Masiero <andreymasiero@gmail.com>
2020-10-12 19:12:11 -0300

    Rotas e View finalizado

---
+++ library/livros/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('livro/<int:livro_id>', views.livro, name='livro'),
+]
--- library/livros/views.py
+++ library/livros/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Livro
 
-# Create your views here.
+def index(request):
+    dados = {
+        'livros': Livro.objects.all()
+    }
+    return render(request, 'index.html', dados)
+
+
+def livro(request, livro_id):
+    livro = {
+        'livro': get_object_or_404(Livro, pk=livro_id)
+    }
+    return render(request, 'livro.html', livro)
